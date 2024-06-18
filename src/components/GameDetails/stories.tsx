ad09e26e248466ea623b0c5bb718e58ccbd283ee
Leonardo Matos <leonardo.matos@ciarama.com.br>
2024-06-18 11:57:51 -0400

    :construction: Create `GameDetails` Component

---
+++ src/components/GameDetails/stories.tsx
@@ -0,0 +1,34 @@
+import { Meta, StoryObj } from '@storybook/react'
+import GameDetails, { GameDetailsProps } from '.'
+import mockGame from './mock'
+export default {
+  title: 'Game/GameDetails',
+  component: GameDetails,
+  parameters: {
+    backgrounds: {
+      default: 'won-dark'
+    }
+  },
+  args: mockGame,
+  argTypes: {
+    releaseDate: {
+      control: 'date'
+    },
+    platforms: {
+      control: 'inline-check',
+      options: ['windows', 'linux', 'mac']
+    },
+    genres: {
+      control: 'inline-check',
+      options: ['Role-playing', 'Narrative']
+    }
+  }
+} as Meta
+
+export const Default: StoryObj<GameDetailsProps> = {
+  render: (args) => (
+    <div style={{ maxWidth: '130rem', margin: '0 auto' }}>
+      <GameDetails {...args} />
+    </div>
+  )
+}
